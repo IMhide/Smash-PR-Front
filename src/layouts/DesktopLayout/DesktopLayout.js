acd3f204f8a91261b972279f851948ec791867e9
hide <sam.hide.ettahiri@gmail.com>
2020-10-06 20:32:31 +0200

    add margin on < 1400px screen

--- src/layouts/DesktopLayout/DesktopLayout.js
+++ src/layouts/DesktopLayout/DesktopLayout.js
@@ -1,8 +1,9 @@
 import React from "react";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import template from "./DesktopLayout.jsx";
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const StylesLarge = makeStyles({
   content: {
     flexGrow: 1,
   },
@@ -11,9 +12,27 @@ const useStyles = makeStyles({
   }
 })
 
+const Styles = makeStyles({
+  content: {
+    flexGrow: 1,
+    marginTop: '85px'
+  },
+  layout: {
+    display: 'flex',
+  }
+})
 
 const DesktopLayout = (props) => {
-  const classes = useStyles()
+  let classes;
+
+
+  const largeScreen = useMediaQuery('(min-width:1400px)');
+  if (largeScreen) {
+    classes = StylesLarge()
+  } else {
+    classes =  Styles()
+  }
+
   return template({ props, classes });
 }
 
