66d8d1e01fadf7dbd9f097bc848503307f0b546d
hide <sam.hide.ettahiri@gmail.com>
2020-10-26 19:34:52 +0100

    first redux step

--- src/components/DesktopSidebar/DesktopSidebar.js
+++ src/components/DesktopSidebar/DesktopSidebar.js
@@ -1,69 +1,17 @@
 import { useEffect, useState } from "react";
 import template from "./DesktopSidebar.jsx";
-import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
+import { StylesMedium, StylesBig } from './DesktopSidebar.style'
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import getMinorCircuits from 'lib/getMinorCircuits'
-import getMajorCircuits from 'lib/getMajorCircuits'
 import getMetaInfo from 'lib/getMetaInfo'
 
-const drawerWidth = 300
+import { useSelector, useDispatch } from "react-redux";
+import { selectCircuit, updateCircuitsAsync} from 'slices/circuits/circuitSlice'
 
-const StylesBig = makeStyles({
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    boxShadow: '0 0 2rem 0 rgba(136,152,170,.15)',
-  },
-  menuIcon: {
-    display: 'none'
-  },
-  appBar: {
-    display: 'none',
-  },
 
-});
-
-const StylesMedium = makeStyles((theme) => ({
-  drawer: {
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    boxShadow: '0 0 2rem 0 rgba(136,152,170,.15)',
-  },
-  menuIcon: {
-    display: 'none'
-  },
-  hide: {
-    display: 'none',
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  bgb: {
-    background: '#000',
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-}));
 
 const DesktopSidebar = (props) => {
   let classes;
-  const [majors, setMajors] = useState([])
-  const [minors, setMinors] = useState([])
   const [metaInfo, setMetaInfo] = useState({ all_time: undefined, current: undefined })
   const [menuClicked, setMenuClicked] = useState(false)
 
@@ -75,6 +23,11 @@ const DesktopSidebar = (props) => {
   } else {
     classes = StylesMedium();
   }
+  
+  const circuits = useSelector(selectCircuit);
+  const dispatch = useDispatch();
+  console.log(circuits)
+
 
   useEffect(() => {
     getMetaInfo().then((response) => {
@@ -86,28 +39,15 @@ const DesktopSidebar = (props) => {
   }, [])
 
   useEffect(() => {
-    getMajorCircuits().then((response) => {
-      const ignoreIds = [parseInt(metaInfo['all_time']), parseInt(metaInfo['current'])]
-      console.log(ignoreIds)
-      const majors = response.data.filter((major) => !ignoreIds.includes(major.id))
-
-      setMajors(majors)
-    }).catch((error) => {
-      console.log('Something went wrong')
-      console.log(error)
-    })
-
-    getMinorCircuits().then((response) => {
-      setMinors(response.data)
-    }).catch((error) => {
-      console.log('Something went wrong')
-      console.log(error)
-    })
-  }, [metaInfo])
+    dispatch(updateCircuitsAsync())
+  }, [])
 
   const handleMenu = () => {
     setMenuClicked(!menuClicked)
   }
+  const ignoreIds = [parseInt(metaInfo['all_time']), parseInt(metaInfo['current'])]
+  const majors = circuits.tournaments.majors.filter((major) => !ignoreIds.includes(major.id))
+  const minors = circuits.tournaments.minors
 
   return template({ classes, majors, minors, metaInfo, drawerOpen, clsx, handleMenu });
 }
