63e58b9a83fb96e767c7bd6065ff5d0d31d64bf3
hide <sam.hide.ettahiri@gmail.com>
2020-11-01 12:48:57 +0100

    deleting console.log

--- src/lib/getCircuitRanking.js
+++ src/lib/getCircuitRanking.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const getCircuitRanking = (circuit_id, placement = false) => {
     const targetParams = placement ? '?placement=true' : ''
-    console.log(process.env)
     const targetHost = process.env.REACT_APP_HOST_TARGET
     const targetURI = `${targetHost}/circuits/${circuit_id}/rankings${targetParams}`
     
